c04c5f5aac40f3217c0a1e5dd773ac2979724d12
XinmanZ <xinman0422@gmail.com>
2023-04-18 15:27:25 -0400

    add ip address to app

--- option1/src/app.py
+++ option1/src/app.py
@@ -1,32 +1,29 @@
-import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect
 
 PORT_NUM = 8888
 HOST = '0.0.0.0'
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    if request.method == 'POST':
-        tool = request.get_json()['tool']
-        if tool == 'hadoop':
-            # do something
-            print(tool)
-        elif tool == 'spark':
-            # do something
-            print(tool)
-        elif tool == 'jupyter':
-            # do something
-            print(tool)
-        elif tool =='sonar':
-            # do something
-            print(tool)
+@app.route('/hadoop')
+def hadoop():
+    return redirect('http://34.67.218.224:9870/')
 
-        return 'OK'
-    else:
-        return render_template('selection.html')
+@app.route('/spark')
+def spark():
+    return redirect('http://35.223.230.207:8080/')
+
+@app.route('/jupyter')
+def jupyter():
+    return redirect('http://34.171.32.232')
+
+@app.route('/sonar')
+def sonar():
+    return redirect('http://34.135.205.155:9000/')
 
 if __name__ == "__main__":
     print("Starting my application...")

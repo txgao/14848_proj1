9b7da96995e2da20742b2d140c6e16aad183b433
txgao <56052188+txgao@users.noreply.github.com>
2023-04-19 20:37:59 -0400

    Delete scanner.sh

--- option1/test/scanner.sh
+++
@@ -1,8 +0,0 @@
-# !bin/bash
-export SERVER_URL=104.196.181.196:30542
-export PASSWORD="secret"
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "login=admin&password=$PASSWORD&previousPassword=admin" -u admin:admin $SERVER_URL/api/users/change_password
-export SONAR_LOGIN=$(curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "name=admin" -u admin:$PASSWORD $SERVER_URL/api/user_tokens/generate)
-echo $SONAR_LOGIN
-SONAR_LOGIN=$(python3 get_token.py $SONAR_LOGIN)
-sonar-scanner -X -Dsonar.projectKey=project -Dsonar.sources=./prj/ -Dsonar.host.url=http://$SERVER_URL -Dsonar.login=$SONAR_LOGIN
